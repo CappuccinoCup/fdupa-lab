74f4bcb7e5ad7ae861233fe81638b8671e82a049
CappuccinoCup <CappuccinoCup@126.com>
2023-10-28 21:12:41 +0800

    lab1 v0.1

--- lib/analysis/intervalAnalysis.cpp
+++ lib/analysis/intervalAnalysis.cpp
@@ -12,19 +12,251 @@ using namespace fdlang::analysis;
  * Your code starts here
  */
 
-void IntervalAnalysis::run() {
-
-    // You can modify this function arbitrarily
-
+void IntervalAnalysis::init() {
     for (auto inst : insts) {
-        if (inst->getInstType() != IR::InstType::CheckIntervalInst)
-            continue;
-
-        IR::CheckIntervalInst *checkInst = (IR::CheckIntervalInst *)inst;
-        std::string variable = checkInst->getOperand(0)->getAsVariable();
-        long long l = checkInst->getOperand(1)->getAsNumber();
-        long long r = checkInst->getOperand(2)->getAsNumber();
-
-        results[checkInst] = ResultType::UNREACHABLE;
+        auto type = inst->getInstType();
+        if (type == IR::InstType::AssignInst) {
+            IR::AssignInst *assignInst = (IR::AssignInst *)inst;
+            auto dest = assignInst->getOperand(0);
+            if (!dest->isVariable()) {
+                continue;
+            }
+            vars.emplace(dest->getAsVariable());
+            auto src = assignInst->getOperand(1);
+            if (src->isVariable()) {
+                vars.emplace(src->getAsVariable());
+            }
+        }
+        if (type == IR::InstType::InputInst) {
+            IR::InputInst *inputInst = (IR::InputInst *)inst;
+            auto dest = inputInst->getOperand(0);
+            if (!dest->isVariable()) {
+                continue;
+            }
+            vars.emplace(dest->getAsVariable());
+        }
+        if (type == IR::InstType::AddInst) {
+            IR::AddInst *addInst = (IR::AddInst *)inst;
+            auto dest = addInst->getOperand(0);
+            if (!dest->isVariable()) {
+                continue;
+            }
+            vars.emplace(dest->getAsVariable());
+            auto op1 = addInst->getOperand(1);
+            if (op1->isVariable()) {
+                vars.emplace(op1->getAsVariable());
+            }
+            auto op2 = addInst->getOperand(2);
+            if (op2->isVariable()) {
+                vars.emplace(op2->getAsVariable());
+            }
+        }
+        if (type == IR::InstType::SubInst) {
+            IR::SubInst *subInst = (IR::SubInst *)inst;
+            auto dest = subInst->getOperand(0);
+            if (!dest->isVariable()) {
+                continue;
+            }
+            vars.emplace(dest->getAsVariable());
+            auto op1 = subInst->getOperand(1);
+            if (op1->isVariable()) {
+                vars.emplace(op1->getAsVariable());
+            }
+            auto op2 = subInst->getOperand(2);
+            if (op2->isVariable()) {
+                vars.emplace(op2->getAsVariable());
+            }
+        }
+        if (type == IR::InstType::GotoInst) {
+            IR::GotoInst *gotoInst = (IR::GotoInst *)inst;
+            targetToJump[gotoInst->getDestInst()->getLabel()] = gotoInst->getLabel();
+            BranchInfo* bInfo = new BranchInfo();
+            branchInfos[gotoInst->getLabel()] = bInfo;
+        }
+        if (type == IR::InstType::IfInst) {
+            IR::IfInst *ifInst = (IR::IfInst *)inst;
+            targetToJump[ifInst->getDestInst()->getLabel()] = ifInst->getLabel();
+            std::string op1 = ifInst->getOperand(0)->getAsVariable();
+            int op2 = ifInst->getOperand(1)->getAsNumber();
+            int s, e;
+            switch (ifInst->getCmpOperator())
+            {
+            case fdlang::IR::CmpOperator::EQ:
+                s = op2;
+                e = op2;
+                break;
+            case fdlang::IR::CmpOperator::GEQ:
+                s = op2;
+                e = 255;
+                break;
+            case fdlang::IR::CmpOperator::GT:
+                s = op2 + 1;
+                e = 255;
+                break;
+            case fdlang::IR::CmpOperator::LEQ:
+                s = 0;
+                e = op2;
+                break;
+            case fdlang::IR::CmpOperator::LT:
+                s = 0;
+                e = op2 - 1;
+                break;
+            default:
+                break;
+            }
+            BranchInfo* bInfo = new BranchInfo(op1, s, e);
+            branchInfos[ifInst->getLabel()] = bInfo;
+            vars.emplace(op1);
+        }
+        if (type == IR::InstType::CheckIntervalInst) {
+            IR::CheckIntervalInst *checkInst = (IR::CheckIntervalInst *)inst;
+            int l = checkInst->getOperand(1)->getAsNumber();
+            int r = checkInst->getOperand(2)->getAsNumber();
+            CheckInfo *cInfo = new CheckInfo(checkInst, l, r);
+            checkInfos[checkInst->getLabel()] = cInfo;
+        }
     }
+}
+
+void IntervalAnalysis::iter() {
+    bool changed = true;
+    while (changed)
+    {
+    // debug
+    std::cout << std::endl << "=======" << std::endl;
+    // debug
+    changed = false;
+    for (auto& v : vars) {
+        currRange.insertVar(v, Range(0, 0));
+    }
+    for (auto inst : insts)
+    {   
+        // debug
+        std::cout << "CurrRange: ";
+        currRange.print();
+        std::cout << std::endl;
+        // debug
+        auto type = inst->getInstType();
+        if (type == IR::InstType::AssignInst) {
+            IR::AssignInst *assignInst = (IR::AssignInst *)inst;
+            auto dest = assignInst->getOperand(0);
+            if (!dest->isVariable()) {
+                continue;
+            }
+            auto src = assignInst->getOperand(1);
+            if (src->isNumber()) {
+                auto value = src->getAsNumber();
+                currRange.insertVar(dest->getAsVariable(), Range(value, value));
+            }
+            if (src->isVariable()) {
+                auto value = src->getAsVariable();
+                currRange.insertVar(dest->getAsVariable(), currRange.getVar(value));
+            }
+        }
+        if (type == IR::InstType::InputInst) {
+            IR::InputInst *inputInst = (IR::InputInst *)inst;
+            auto dest = inputInst->getOperand(0);
+            if (!dest->isVariable()) {
+                continue;
+            }
+            currRange.insertVar(dest->getAsVariable(), Range(0, 255));
+        }
+        if (type == IR::InstType::AddInst) {
+            IR::AddInst *addInst = (IR::AddInst *)inst;
+            auto dest = addInst->getOperand(0);
+            if (!dest->isVariable()) {
+                continue;
+            }
+            auto op1 = addInst->getOperand(1);
+            auto op2 = addInst->getOperand(2);
+            Range op1R, op2R;
+            if (op1->isNumber()) {
+                op1R = Range(op1->getAsNumber(), op1->getAsNumber());
+            }
+            if (op1->isVariable()) {
+                op1R = currRange.getVar(op1->getAsVariable());
+            }
+            if (op2->isNumber()) {
+                op2R = Range(op2->getAsNumber(), op2->getAsNumber());
+            }
+            if (op2->isVariable()) {
+                op2R = currRange.getVar(op2->getAsVariable());
+            }
+            op1R.range_add(op2R);
+            currRange.insertVar(dest->getAsVariable(), op1R);
+        }
+        if (type == IR::InstType::SubInst) {
+            IR::SubInst *subInst = (IR::SubInst *)inst;
+            auto dest = subInst->getOperand(0);
+            if (!dest->isVariable()) {
+                continue;
+            }
+            auto op1 = subInst->getOperand(1);
+            auto op2 = subInst->getOperand(2);
+            Range op1R, op2R;
+            if (op1->isNumber()) {
+                op1R = Range(op1->getAsNumber(), op1->getAsNumber());
+            }
+            if (op1->isVariable()) {
+                op1R = currRange.getVar(op1->getAsVariable());
+            }
+            if (op2->isNumber()) {
+                op2R = Range(op2->getAsNumber(), op2->getAsNumber());
+            }
+            if (op2->isVariable()) {
+                op2R = currRange.getVar(op2->getAsVariable());
+            }
+            op1R.range_minus(op2R);
+            currRange.insertVar(dest->getAsVariable(), op1R);
+        }
+        if (type == IR::InstType::GotoInst) {
+            IR::GotoInst *gotoInst = (IR::GotoInst *)inst;
+            auto bInfo = branchInfos[gotoInst->getLabel()];
+            changed = changed || bInfo->setGotoRange(currRange);
+            if (gotoInst->getDestInst()->getLabel() > gotoInst->getLabel()) {
+                // Only jump forward needs a new iteration
+                changed = false;
+            }
+        }
+        if (type == IR::InstType::IfInst) {
+            IR::IfInst *ifInst = (IR::IfInst *)inst;
+            auto bInfo = branchInfos[ifInst->getLabel()];
+            changed = changed || bInfo->setIfRange(currRange);
+            if (ifInst->getDestInst()->getLabel() > ifInst->getLabel()) {
+                // Only jump forward needs a new iteration
+                changed = false;
+            }
+        }
+        if (type == IR::InstType::LabelInst) {
+            IR::LabelInst *labelInst = (IR::LabelInst *)inst;
+            auto jumpRange = branchInfos[targetToJump[labelInst->getLabel()]]->getJumpRange();
+            currRange.range_union(jumpRange);
+        }
+        if (type == IR::InstType::CheckIntervalInst) {
+            IR::CheckIntervalInst *checkInst = (IR::CheckIntervalInst *)inst;
+            auto cInfo = checkInfos[checkInst->getLabel()];
+            cInfo->updateRealRange(currRange);
+        }
+    }
+    }
+}
+
+void IntervalAnalysis::done() {
+    for (auto& cInfo : checkInfos) {
+        results[cInfo.second->getInst()] = cInfo.second->getResultType();
+    }
+    for (auto& bInfo : branchInfos) {
+        delete bInfo.second;
+    }
+    branchInfos.clear();
+    for (auto& cInfo : checkInfos) {
+        delete cInfo.second;
+    }
+    checkInfos.clear();
+}
+
+void IntervalAnalysis::run() {
+    init();
+    iter();
+    done();
 }
--- lib/analysis/intervalAnalysis.h
+++ lib/analysis/intervalAnalysis.h
@@ -6,10 +6,430 @@
 #include <algorithm>
 #include <map>
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
 
 namespace fdlang::analysis {
 
 enum class ResultType { YES, NO, UNREACHABLE };
+
+/**
+ * Range of a variable
+ * [x: {a, b}, {c, d}] means x in [a, b] v [c, d]
+*/
+class Range {
+private:
+    static constexpr int LOWER_BOUND = 0;
+    static constexpr int UPPER_BOUND = 255;
+    std::vector<std::pair<int, int>> range;
+
+    std::vector<std::pair<int, int>> getRange() {
+        return range;
+    }
+
+    void add(int s, int e) {
+        s = std::max(s, LOWER_BOUND);
+        e = std::min(e, UPPER_BOUND);
+        if (s <= e) {
+            range.emplace_back(s, e);
+        }
+    }
+
+    void complement() {
+        Range newRange;
+        int ele = LOWER_BOUND;
+        for (auto& r : range) {
+            newRange.add(ele, r.first - 1);
+            ele = r.second + 1;
+        }
+        newRange.add(ele, UPPER_BOUND);
+        newRange.arrange();
+        range = newRange.getRange();
+    }
+
+    void arrange() {
+        if (range.empty()) {
+            return;
+        }
+        sort(range.begin(), range.end());
+        std::vector<std::pair<int, int>> newRange;
+        size_t size = range.size();
+        auto& r = range[0];
+        for (int i = 1; i < size; i++) {
+            auto& _r = range[i];
+            if (_r.first <= r.second + 1) {
+                r.second = std::max(r.second, _r.second);
+            } else {
+                newRange.push_back(r);
+                r = _r;
+            }
+        }
+        newRange.push_back(r);
+        range = newRange;
+    }
+
+public:
+    Range() = default;
+
+    Range(int s, int e) {
+        this->add(s, e);
+    }
+
+    ~Range() = default;
+
+    // debug
+    void print() {
+        for (auto& r : range) {
+            std::cout << "[" << r.first << ", " << r.second << "]";
+        }
+    }
+    // debug
+
+    // set range = range v _range
+    // return true if range is changed
+    bool range_union(Range& _range) {
+        Range newRange = *this;
+        for (auto& _r : _range.getRange()) {
+            newRange.add(_r.first, _r.second);
+        }
+        newRange.arrange();
+        if (!this->range_equal(newRange)) {
+            range = newRange.getRange();
+            return true;
+        }
+        return false;
+    }
+
+    // set range = range ^ _range
+    // return true if range is changed
+    bool range_join(Range& _range) {
+        if (this->is_empty()) {
+            return false;
+        }
+        Range newRange;
+        int idx = 0;
+        for (auto& _r : _range.getRange()) {
+            while (idx < range.size() && range[idx].second < _r.first) {
+                idx++;
+            }
+            while (idx < range.size() && range[idx].second <= _r.second) {
+                auto& r = range[idx];
+                newRange.add(std::max(_r.first, r.first), std::min(_r.second, r.second));
+                idx++;
+            }
+            if (idx < range.size()) {
+                auto& r = range[idx];
+                newRange.add(std::max(_r.first, r.first), std::min(_r.second, r.second));
+            }
+        }
+        newRange.arrange();
+        if (!this->range_equal(newRange)) {
+            range = newRange.getRange();
+            return true;
+        }
+        return false;
+    }
+
+    // set range = range - _range
+    // return true if range is changed
+    bool range_subtract(Range& _range) {
+        if (this->is_empty()) {
+            return false;
+        }
+        Range newRange = _range;
+        newRange.complement();
+        newRange.range_join(*this);
+        newRange.arrange();
+        if (!this->range_equal(newRange)) {
+            range = newRange.getRange();
+            return true;
+        }
+        return false;
+    }
+
+    // extend range by an integer
+    void range_add(int c) {
+        for (auto& r : range) {
+            r.first = std::min(r.first + c, 255);
+            r.second = std::min(r.second + c, 255);
+        }
+        arrange();
+    }
+
+    // extend range by a range
+    void range_add(Range& _range) {
+        for (auto& _r : _range.getRange()) {
+            for (auto& r : range) {
+                r.first = std::min(r.first + _r.first, 255);
+                r.second = std::min(r.second + _r.second, 255);
+            }
+        }
+        arrange();
+    }
+
+    // reduce range by an integer
+    void range_minus(int c) {
+        for (auto& r : range) {
+            r.first = std::max(r.first - c, 0);
+            r.second = std::min(r.second - c, 0);
+        }
+        arrange();
+    }
+
+    // reduce range by a range
+    void range_minus(Range& _range) {
+        for (auto& _r : _range.getRange()) {
+            for (auto& r : range) {
+                r.first = std::max(r.first - _r.second, 0);
+                r.second = std::max(r.second - _r.first, 0);
+            }
+        }
+        arrange();
+    }
+
+    // compare range and _range
+    // return true if range is equal to _range
+    bool range_equal(Range& _range) {
+        size_t rSize = range.size();
+        auto _r = _range.getRange();
+        if (rSize != _r.size()) {
+            return false;
+        }
+        for (int i = 0; i < rSize; i++) {
+            if (range[i].first != _r[i].first || range[i].second != _r[i].second) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // return true if range is a subset of _range
+    bool is_subset_of(Range& _range) {
+        int _idx = 0;
+        auto _r = _range.getRange();
+        for (auto& r : range) {
+            while (_idx < _r.size() && _r[_idx].second < r.first) {
+                _idx++;
+            }
+            if (_idx == _r.size() || _r[_idx].first > r.first || _r[_idx].second < r.second) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // return true if range is an empty set
+    bool is_empty() {
+        return range.empty();
+    }
+};
+
+/**
+ * Range of multiple variables
+*/
+class VarRange {
+private:
+    std::unordered_map<std::string, Range> varRange;
+
+    bool containVar(const std::string& var) {
+        return varRange.find(var) != varRange.end();
+    }
+
+public:
+    VarRange() = default;
+    ~VarRange() = default;
+
+    // debug
+    void print() {
+        for (auto& vr : varRange) {
+            std::cout << vr.first << ": ";
+            vr.second.print();
+            std::cout << " ";
+        }
+    }
+    // debug
+
+    Range getVar(const std::string& var) {
+        return varRange[var];
+    }
+
+    std::unordered_set<std::string> getVarSet() {
+        std::unordered_set<std::string> res;
+        for (auto& vr : varRange) {
+            res.emplace(vr.first);
+        }
+        return res;
+    }
+
+    void insertVar(const std::string& var, Range range) {
+        if (!range.is_empty()) {
+            varRange[var] = range;
+        }
+    }
+
+    bool range_union(VarRange& _varRange) {
+        bool changed = false;
+        auto _varSet = _varRange.getVarSet();
+        for (auto& _v : _varSet) {
+            if (this->containVar(_v)) {
+                auto _range = _varRange.getVar(_v);
+                changed = changed | varRange[_v].range_union(_range);
+            } else {
+                this->insertVar(_v, _varRange.getVar(_v));
+                changed = true;
+            }
+        }
+        return changed;
+    }
+
+    bool range_join(VarRange& _varRange) {
+        bool changed = false;
+        auto _varSet = _varRange.getVarSet();
+        for (auto& _v : _varSet) {
+            if (this->containVar(_v)) {
+                auto _range = _varRange.getVar(_v);
+                changed = changed | varRange[_v].range_join(_range);
+            }
+        }
+        return changed;
+    }
+
+    bool range_subtract(VarRange& _varRange) {
+        bool changed = false;
+        auto _varSet = _varRange.getVarSet();
+        for (auto& _v : _varSet) {
+            if (this->containVar(_v)) {
+                auto _range = _varRange.getVar(_v);
+                changed = changed | varRange[_v].range_subtract(_range);
+            }
+        }
+        return changed;
+    }
+
+    bool range_equal(VarRange& _varRange) {
+        auto varSet = _varRange.getVarSet();
+        for (auto& _v : varSet) {
+            if (!this->containVar(_v)) {
+                return false;
+            }
+            auto _vr = _varRange.getVar(_v);
+            if (!varRange[_v].range_equal(_vr)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+/**
+ * Information of branch IR (Goto & If)
+*/
+class BranchInfo {
+private:
+    VarRange jumpRange;     // Range of variables when this IR jump
+    std::string condX;      // Condition variable
+    Range condRange;        // Range of condition
+
+public:
+    BranchInfo() = default;
+
+    BranchInfo(std::string x, int s, int e) {
+        condX = std::move(x);
+        condRange = Range(s, e);
+    }
+
+    ~BranchInfo() = default;
+
+    VarRange getJumpRange() {
+        return jumpRange;
+    }
+
+    bool setGotoRange(VarRange& currRange) {
+        bool changed = false;
+        changed = !jumpRange.range_equal(currRange);
+        if (changed) {
+            jumpRange = currRange;
+        }
+        currRange = VarRange();
+        return changed;
+    }
+
+    bool setIfRange(VarRange& currRange) {
+        bool changed = false;
+
+        auto jxRange = jumpRange.getVar(condX);
+        auto newJXRange = currRange.getVar(condX);
+        newJXRange.range_join(condRange);
+        if (newJXRange.is_empty()) {
+            if (!jumpRange.getVarSet().empty()) {
+                jumpRange = VarRange();
+                changed = true;
+            }
+        } else if (!jxRange.range_equal(newJXRange)) {
+            auto tmp = currRange;
+            tmp.insertVar(condX, newJXRange);
+            jumpRange = tmp;
+            changed = true;
+        }
+
+        // std::cout << "New Jump Range: " << std::endl;
+        // jumpRange.print();
+
+        auto newXRange = currRange.getVar(condX);
+        newXRange.range_subtract(newJXRange);
+        if (newXRange.is_empty()) {
+            if (!currRange.getVarSet().empty()) {
+                currRange = VarRange();
+            }
+        } else {
+            auto tmp = VarRange();
+            tmp.insertVar(condX, condRange);
+            tmp.range_join(currRange);
+            currRange.range_subtract(tmp);
+        }
+
+        // std::cout << "New No Jump Range: " << std::endl;
+        // currRange.print();
+
+        return changed;
+    }
+};
+
+/**
+ * Information of CheckInterval IR
+*/
+class CheckInfo {
+private:
+    fdlang::IR::CheckIntervalInst* inst;    // This CheckInterval IR
+    Range checkRange;                       // Variable range this IR expected
+    Range realRange;                        // Variable range when this IR is executed 
+
+public:
+    CheckInfo(fdlang::IR::CheckIntervalInst* inst, int s, int e) {
+        this->inst = inst;
+        checkRange = Range(s, e);
+    }
+
+    ~CheckInfo() = default;
+
+    fdlang::IR::CheckIntervalInst* getInst() {
+        return inst;
+    }
+
+    void updateRealRange(VarRange& currRange) {
+        realRange = currRange.getVar(inst->getOperand(0)->getAsVariable());
+    }
+
+    ResultType getResultType() {
+        if (realRange.is_empty()) {
+            return ResultType::UNREACHABLE;
+        }
+        if (realRange.is_subset_of(checkRange)) {
+            return ResultType::YES;
+        }
+        return ResultType::NO;
+    }
+};
 
 class IntervalAnalysis : public DataflowAnalysis {
 private:
@@ -50,6 +470,18 @@ private:
     /**
      * Your code starts here
      */
+
+    // Analysis structures
+    std::unordered_map<int, int> targetToJump;          // Jump target to jump IR
+    std::unordered_map<int, BranchInfo*> branchInfos;   // Jump IR label to branch info
+    std::unordered_map<int, CheckInfo*> checkInfos;     // CheckInterval IR label to check info
+    std::unordered_set<std::string> vars;               // All variables
+    VarRange currRange;                                 // Current range of all variables
+    
+    // Analysis passes
+    void init();                                        // Prepare analysis structures
+    void iter();                                        // Iterate util branchInfos stable
+    void done();                                        // Write results and tear down
 };
 
 } // namespace fdlang::analysis
