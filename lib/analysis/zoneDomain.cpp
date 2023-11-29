970f3eea5c49296a75df1f4cd9fb600f35748aea
CappuccinoCup <CappuccinoCup@126.com>
2023-11-29 18:59:04 +0800

    Fix upstream merging

--- lib/analysis/zoneDomain.cpp
+++ lib/analysis/zoneDomain.cpp
@@ -405,7 +405,6 @@ ZoneDomain ZoneDomain::assign_case3(const std::string &x, long long l,
                                     long long r) const {
     l = std::max(l, 0ll);
     r = std::min(r, 255ll);
-    ZoneDomain ret;
 
     // todo: (about 4 lines)
     ZoneDomain ret = this->forget(x);
